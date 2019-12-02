d8bfc673df3794cf0e09880b20685aff27e30584
S.M. Amit Hasan <47453416+bdamit007@users.noreply.github.com>
2019-12-02 12:08:07 +0600

    Update new.cpp

--- new.cpp
+++ new.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
- 
 using namespace std;
- 
 int main()
 {
-  // print output to user
-  cout << "Hello World!" << endl;
+  int n, i;
+  bool isPrime = true;
+  cout << "Enter a positive integer: ";
+  cin >> n;
+  for(i = 2; i <= n / 2; ++i)
+  {
+      if(n % i == 0)
+      {
+          isPrime = false;
+          break;
+      }
+  }
+  if (isPrime)
+      cout << "This is a prime number";
+  else
+      cout << "This is not a prime number";
   return 0;
-  cout >>
+ 
+ cout >>
+ 
+       cout << "This is not a prime number";
+  return 0;
 }
